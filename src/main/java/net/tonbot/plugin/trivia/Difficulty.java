3a3f7abd3095de61cfb364736c213c891f2bb2fb
James Li <lijamez@users.noreply.github.com>
2018-02-10 01:57:19 -0800

    Adding missing file.

---
+++ src/main/java/net/tonbot/plugin/trivia/Difficulty.java
@@ -0,0 +1,18 @@
+package net.tonbot.plugin.trivia;
+
+enum Difficulty {
+
+	EASY("Easy"),
+	MEDIUM("Medium"),
+	HARD("Hard");
+
+	private final String friendlyName;
+
+	private Difficulty(String friendlyName) {
+		this.friendlyName = friendlyName;
+	}
+
+	public String getFriendlyName() {
+		return friendlyName;
+	}
+}
