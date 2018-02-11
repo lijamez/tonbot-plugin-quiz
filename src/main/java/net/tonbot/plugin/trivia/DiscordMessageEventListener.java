c691c0fa782c87893963b32b7291cfb7fa379743
James Li <lijamez@users.noreply.github.com>
2018-02-10 17:42:45 -0800

    cleanup

--- src/main/java/net/tonbot/plugin/trivia/DiscordMessageEventListener.java
+++ src/main/java/net/tonbot/plugin/trivia/DiscordMessageEventListener.java
@@ -6,26 +6,19 @@ import org.apache.commons.lang3.StringUtils;
 
 import com.google.common.base.Preconditions;
 import com.google.inject.Inject;
-import com.vdurmont.emoji.EmojiParser;
 
 import net.tonbot.common.MessageNormalizer;
-import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
 import sx.blah.discord.handle.obj.IMessage;
-import sx.blah.discord.util.MessageTokenizer;
 
 class DiscordMessageEventListener {
 
-	private final IDiscordClient discordClient;
 	private final TriviaSessionManager triviaSessionManager;
 
 	@Inject
 	public DiscordMessageEventListener(
-			IDiscordClient discordClient,
 			TriviaSessionManager triviaSessionManager) {
-		this.discordClient = Preconditions.checkNotNull(discordClient,
-				"discordClient must be non-null.");
 		this.triviaSessionManager = Preconditions.checkNotNull(triviaSessionManager,
 				"triviaSessionManager must be non-null.");
 	}
@@ -49,13 +42,13 @@ class DiscordMessageEventListener {
 
 		TriviaSession session = optSession.get();
 		IMessage message = messageReceivedEvent.getMessage();
-		
+
 		String normalizedMessageContent = normalize(message.getContent());
-		
+
 		if (StringUtils.isBlank(normalizedMessageContent)) {
 			return;
 		}
-		
+
 		UserMessage userMessage = UserMessage.builder()
 				.message(normalizedMessageContent)
 				.messageId(message.getLongID())
@@ -63,10 +56,10 @@ class DiscordMessageEventListener {
 				.build();
 		session.takeInput(userMessage);
 	}
-	
+
 	private String normalize(String message) {
 		return MessageNormalizer.removeEmojis(message)
 				.trim();
-				
+
 	}
 }
